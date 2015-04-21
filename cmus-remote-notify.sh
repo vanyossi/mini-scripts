7edf39f39f3c42f11ffcc7a2922a797ee7c2c8b6
Ivan Yossi <ghevan@gmail.com>
2015-04-21 18:37:20 -0500

    FORMAT: use spaces instead of tabs

--- cmus-remote-notify.sh
+++ cmus-remote-notify.sh
@@ -5,95 +5,95 @@
 # check argument is present
 if test $# -eq 0
 then
-	echo 'Script needs at least one option!'
-	exit
+    echo 'Script needs at least one option!'
+    exit
 fi
 
 Cmus_remote=$(cmus-remote -Q)
 
 # return index after given string
 get_position () {
-	local search_string=$@
+    local search_string=$@
 
-	while read -r line
-	do
-		index=$(expr match "$line" "$search_string")
-		if [ $index -gt 0 ]
-		then
-			break
-		fi
-	done <<< "$Cmus_remote"
+    while read -r line
+    do
+        index=$(expr match "$line" "$search_string")
+        if [ $index -gt 0 ]
+        then
+            break
+        fi
+    done <<< "$Cmus_remote"
 
-	return $index
+    return $index
 }
 
 
 if test ${#Cmus_remote} -eq 0
 then
-	# xfce4-terminal --maximize --hide-menubar -x cmus &&
-  	# exo-open --launch TerminalEmulator cmus &&
-	sleep 2
-	# cmus-remote -p
+    # xfce4-terminal --maximize --hide-menubar -x cmus &&
+    # exo-open --launch TerminalEmulator cmus &&
+    sleep 2
+    # cmus-remote -p
 else
-	if test $1 = "-S"
-	then
-		Shuffle="Shuffle On"
+    if test $1 = "-S"
+    then
+        Shuffle="Shuffle On"
 
-		get_position "set shuffle"
-		shuffle_is=${line:$?}
+        get_position "set shuffle"
+        shuffle_is=${line:$?}
 
-		if test $shuffle_is = "true"
-		then
-			Shuffle="Shuffle Off"
-		fi
-		notify-send -t 1400 "Cmus notifier" "$Shuffle"
+        if test $shuffle_is = "true"
+        then
+            Shuffle="Shuffle Off"
+        fi
+        notify-send -t 1400 "Cmus notifier" "$Shuffle"
 
-	elif test $1 = "-r"
-	then
+    elif test $1 = "-r"
+    then
 
-		get_position "position"
-		position=${line:$?}
+        get_position "position"
+        position=${line:$?}
 
-		if test $position -gt 10
-		then
-			# overwrite arguments to new position
-			set -- "-p"
-		fi
-	fi
+        if test $position -gt 10
+        then
+            # overwrite arguments to new position
+            set -- "-p"
+        fi
+    fi
 
-	if test $1 != "-Q"
-	then
-		cmus-remote $1
-	fi
+    if test $1 != "-Q"
+    then
+        cmus-remote $1
+    fi
 
-	# get current data and declare associative array
-	Cur_song=$(cmus-remote -Q)
-	declare -A song_tags
+    # get current data and declare associative array
+    Cur_song=$(cmus-remote -Q)
+    declare -A song_tags
 
-	# Go trough each line and populate associative array
-	while read -r line
-	do
-    	pos=$(expr "$line" : '[a-z]*. ')
-    	if test $pos -eq 4
-    	then
-    		tag_info=${line:$pos}
-    		tag_pos=$(expr "$tag_info" : '[a-z_]*. ')
-    		tag=${tag_info:0:$tag_pos-1}
+    # Go trough each line and populate associative array
+    while read -r line
+    do
+        pos=$(expr "$line" : '[a-z]*. ')
+        if test $pos -eq 4
+        then
+            tag_info=${line:$pos}
+            tag_pos=$(expr "$tag_info" : '[a-z_]*. ')
+            tag=${tag_info:0:$tag_pos-1}
 
-    		if test $tag != ' '
-    		then
-    			song_tags[$tag]=${tag_info:$tag_pos}
-    		fi
-    	fi
-	done <<< "$Cur_song"
+            if test $tag != ' '
+            then
+                song_tags[$tag]=${tag_info:$tag_pos}
+            fi
+        fi
+    done <<< "$Cur_song"
 
-	if test $1 = "-Q"
-	then
-		cmus-updatepidgin artist "${song_tags[artist]}" title "${song_tags[title]}"
-	fi
+    if test $1 = "-Q"
+    then
+        cmus-updatepidgin artist "${song_tags[artist]}" title "${song_tags[title]}"
+    fi
 
-	if test $1 != "-S"
-	then
-		notify-send -i multimedia-volume-control -t 2200 "${song_tags[title]}" "${song_tags[album]}\n${song_tags[artist]}"
-	fi
+    if test $1 != "-S"
+    then
+        notify-send -i multimedia-volume-control -t 2200 "${song_tags[title]}" "${song_tags[album]}\n${song_tags[artist]}"
+    fi
 fi
--- nvxfce.sh
+++ nvxfce.sh
@@ -6,33 +6,33 @@ zimConfig="${HOME}/.config/zim/notebooks.list"
 
 # Save selected notbook and rearrange args.
 if test $(expr $1 : '[-].*') = 0
-	then
-	let notebook="$1"
-	set -- "${@:2:${#@}}"
+    then
+    let notebook="$1"
+    set -- "${@:2:${#@}}"
 else
-	notebook="D"
+    notebook="D"
 fi
 
 while read -r line
 do
-	if test "${line::1}" = "$notebook"
-	then
-		# echo "$line ${line::1}"
-		index=$(expr "$line" : '.*.=.')
-		noteDir=${HOME}${line:$index}
-		# ger last string after '/'
-		noteName=${line##*/}
-	fi
-	# echo $line
+    if test "${line::1}" = "$notebook"
+    then
+        # echo "$line ${line::1}"
+        index=$(expr "$line" : '.*.=.')
+        noteDir=${HOME}${line:$index}
+        # ger last string after '/'
+        noteName=${line##*/}
+    fi
+    # echo $line
 done < $zimConfig
 
 cd $noteDir
 # ========================
 
-_run_noteApp()	
+_run_noteApp()  
 {
-	nohup zim --standalone $noteName "$1" & disown
-	exit
+    nohup zim --standalone $noteName "$1" & disown
+    exit
 }
 
 # USAGE: nvxfce.sh [Notebook number] [option] String to search
@@ -45,30 +45,30 @@ let i=1
 # if first argument "-c" searches content and not title
 if test $1 = "-c"
 then
-	search_str="${@:2:${#@}}"
-	for filename in $(grep -Hrli "$search_str" *)
-	do
-		list[$i]="${filename}"
-		let i++
-	done
+    search_str="${@:2:${#@}}"
+    for filename in $(grep -Hrli "$search_str" *)
+    do
+        list[$i]="${filename}"
+        let i++
+    done
 
 elif test $1 = "-s"
 then
-	# search and replace spaces for '_'
-	printf -v nospace "_%s" "${@:2:${#@}}"
-	nospace=${nospace:1}
+    # search and replace spaces for '_'
+    printf -v nospace "_%s" "${@:2:${#@}}"
+    nospace=${nospace:1}
 
-	for filename in *
-	do
-		if test $(expr match "$filename" "$nospace") -gt 0
-			then
-			list[$i]="${filename}"
-			let i++
-		fi
-	done
-	# printf "%s\n" ${list[@]}
+    for filename in *
+    do
+        if test $(expr match "$filename" "$nospace") -gt 0
+            then
+            list[$i]="${filename}"
+            let i++
+        fi
+    done
+    # printf "%s\n" ${list[@]}
 else
-	_run_noteApp "$1"
+    _run_noteApp "$1"
 fi
 
 # reset counter
@@ -76,22 +76,22 @@ let i=0
 
 # List all containing search string
 for f in ${list[@]};
-	do
-	let i++
-	printf "%3d %s\n" $i $f
+    do
+    let i++
+    printf "%3d %s\n" $i $f
 done
 
 # exits program if no results
 if test $i = "0"
-	then
-	echo "No results found!"
-	exit
+    then
+    echo "No results found!"
+    exit
 
 # if only one item in list, select it
 elif test $i = "1"
 then
-	_run_noteApp ${list[1]%.txt}
-	exit
+    _run_noteApp ${list[1]%.txt}
+    exit
 fi
 
 # promt user for selection
@@ -102,7 +102,7 @@ read selection
 # this ensures at least one valid selection.
 if test -z "$selection"
 then
-	selection=1
+    selection=1
 fi
 
 _run_noteApp ${list[$selection]%.txt}
