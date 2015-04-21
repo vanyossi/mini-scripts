3a23d9078caf88929af1f618ae28b8c4830e6847
Ivan Yossi <ghevan@gmail.com>
2015-04-21 14:43:35 -0500

    cnotify: use only core bash functions to get data fix format in notifications restart current song if more than 10 sec are played

--- cmus-remote-notify.sh
+++ cmus-remote-notify.sh
@@ -1,29 +1,68 @@
 #!/bin/sh
+# cmus--remote-notify.sh
+
+# ${1?"Usage: $0 ARGUMENT"}
+# check argument is present
+if test $# -eq 0
+then
+	echo 'Script needs at least one option!'
+	exit
+fi
+
 Cmus_remote=$(cmus-remote -Q)
 
-Instance=$(echo -e "$Cmus_remote" | wc -l)
-if [ $Instance = 1 ]; then
+# return index after given string
+get_position () {
+	local search_string=$@
+
+	while read -r line
+	do
+		index=$(expr match "$line" "$search_string")
+		if [ $index -gt 0 ]
+		then
+			break
+		fi
+	done <<< "$Cmus_remote"
+
+	return $index
+}
+
+
+if test ${#Cmus_remote} -eq 0
+then
 	# xfce4-terminal --maximize --hide-menubar -x cmus &&
-  	# exo-open --launch TerminalEmulator --fullscreen cmus &&
-	terminal -e cmus &&
+  	# exo-open --launch TerminalEmulator cmus &&
 	sleep 2
-	cmus-remote -p
+	# cmus-remote -p
 else
-	if [ $1 == "-S" ]; then
+	if test $1 = "-S"
+	then
 		Shuffle="Shuffle On"
-		if [ `echo "$Cmus_remote" | grep shuffle | cut -d ' ' -f 3` == true ]; then
+
+		get_position "set shuffle"
+		shuffle_is=${line:$?}
+
+		if test $shuffle_is = "true"
+		then
 			Shuffle="Shuffle Off"
 		fi
-		notify-send -t 800 "Cmus notifies:" "$Shuffle"
-	elif [ $1 == "-r" ]; then
-		position=$(echo "$Cmus_remote" | grep position | cut -d ' ' -f 2)
-		if [ $position -gt 10 ]; then
+		notify-send -t 1400 "Cmus notifier" "$Shuffle"
+
+	elif test $1 = "-r"
+	then
+
+		get_position "position"
+		position=${line:$?}
+
+		if test $position -gt 10
+		then
 			# overwrite arguments to new position
 			set -- "-p"
 		fi
 	fi
 
-	if [ $1 != "-Q" ]; then
+	if test $1 != "-Q"
+	then
 		cmus-remote $1
 	fi
 
@@ -32,21 +71,29 @@ else
 	declare -A song_tags
 
 	# Go trough each line and populate associative array
-	while read -r line; do
+	while read -r line
+	do
     	pos=$(expr "$line" : '[a-z]*. ')
-    	if [ $pos -eq 4 ]; then
+    	if test $pos -eq 4
+    	then
     		tag_info=${line:$pos}
     		tag_pos=$(expr "$tag_info" : '[a-z_]*. ')
     		tag=${tag_info:0:$tag_pos-1}
-    		if [ $tag != ' ' ]; then
+
+    		if test $tag != ' '
+    		then
     			song_tags[$tag]=${tag_info:$tag_pos}
     		fi
     	fi
 	done <<< "$Cur_song"
 
-	if [ $1 == "-Q" ]; then
+	if test $1 = "-Q"
+	then
 		cmus-updatepidgin artist "${song_tags[artist]}" title "${song_tags[title]}"
 	fi
 
-	notify-send -i multimedia-volume-control -t 2200 "${song_tags[title]}" "${song_tags[album]}\n${song_tags[artist]}"
+	if test $1 != "-S"
+	then
+		notify-send -i multimedia-volume-control -t 2200 "${song_tags[title]}" "${song_tags[album]}\n${song_tags[artist]}"
+	fi
 fi
