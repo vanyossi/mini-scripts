0f6f213ebaa65a95c0b482a572ba4398276943d4
Ivan Yossi <ghevan@gmail.com>
2015-04-21 02:04:53 -0500

    Start migrating cmus-notify to use less outside progs and more bash inner functions

--- cmus-remote-notify.sh
+++ cmus-remote-notify.sh
@@ -1,27 +1,52 @@
 #!/bin/sh
 Cmus_remote=$(cmus-remote -Q)
+
 Instance=$(echo -e "$Cmus_remote" | wc -l)
-Shuffle="Shuffle On"
 if [ $Instance = 1 ]; then
+	# xfce4-terminal --maximize --hide-menubar -x cmus &&
+  	# exo-open --launch TerminalEmulator --fullscreen cmus &&
 	terminal -e cmus &&
 	sleep 2
 	cmus-remote -p
 else
 	if [ $1 == "-S" ]; then
+		Shuffle="Shuffle On"
 		if [ `echo "$Cmus_remote" | grep shuffle | cut -d ' ' -f 3` == true ]; then
 			Shuffle="Shuffle Off"
 		fi
-		cmus-remote $1
-		notify-send -t 800 $Shuffle
-	elif [ $1 == "-Q" ]; then
-		Cur_song=$(echo "$Cmus_remote" | grep tag | head -n 3 | sort -r | cut -d ' ' -f 3- )
-		notify-send -i multimedia-volume-control -t 1800 "$Cur_song"
-		artist=$(echo -e "$Cur_song" | head -n 2 | tail -n 1)
-		title=$(echo -e "$Cur_song" | head -n 1 )
-		cmus-updatepidgin artist "$artist" title "$title"
-	else
-		cmus-remote $1
-		Cur_song=$(cmus-remote -Q | grep tag | head -n 3 | sort -r | cut -d ' ' -f 3- )
-		notify-send -i multimedia-volume-control -t 1800 "$Cur_song"
+		notify-send -t 800 "Cmus notifies:" "$Shuffle"
+	elif [ $1 == "-r" ]; then
+		position=$(echo "$Cmus_remote" | grep position | cut -d ' ' -f 2)
+		if [ $position -gt 10 ]; then
+			# overwrite arguments to new position
+			set -- "-p"
+		fi
 	fi
+
+	if [ $1 != "-Q" ]; then
+		cmus-remote $1
+	fi
+
+	# get current data and declare associative array
+	Cur_song=$(cmus-remote -Q)
+	declare -A song_tags
+
+	# Go trough each line and populate associative array
+	while read -r line; do
+    	pos=$(expr "$line" : '[a-z]*. ')
+    	if [ $pos -eq 4 ]; then
+    		tag_info=${line:$pos}
+    		tag_pos=$(expr "$tag_info" : '[a-z_]*. ')
+    		tag=${tag_info:0:$tag_pos-1}
+    		if [ $tag != ' ' ]; then
+    			song_tags[$tag]=${tag_info:$tag_pos}
+    		fi
+    	fi
+	done <<< "$Cur_song"
+
+	if [ $1 == "-Q" ]; then
+		cmus-updatepidgin artist "${song_tags[artist]}" title "${song_tags[title]}"
+	fi
+
+	notify-send -i multimedia-volume-control -t 2200 "${song_tags[title]}" "${song_tags[album]}\n${song_tags[artist]}"
 fi
